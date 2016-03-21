8ce75309222654c843e1aef541410644a39371a1
Cody Simms <codyjsimms@gmail.com>
2016-03-20 23:40:38 -0500

    Move setController to its own method

--- sample/sample7-container/app.ts
+++ sample/sample7-container/app.ts
@@ -1,9 +1,12 @@
 import * as express from 'express';
-import {registerActionsInExpressApp} from '../../src/Factory';
+import {setContainer, registerActionsInExpressApp} from '../../src/Factory';
 import kernel from './kernel';
 import ControllerContainer from './ControllerContainer';
 
 let app = express();
-registerActionsInExpressApp(app, [`${__dirname}/controllers`], new ControllerContainer(kernel));
+
+setContainer(new ControllerContainer(kernel));
+registerActionsInExpressApp(app);
+
 app.listen(3001, 'localhost');
-console.log('Listening on port 3001. Visit http://localhost;3001/respond');
+console.log('Listening on port 3001. Visit http://localhost:3001/respond');
--- src/Factory.ts
+++ src/Factory.ts
@@ -2,16 +2,27 @@ import {ControllerRunner} from "./ControllerRunner";
 import {ExpressServer} from "./server/ExpressServer";
 import {Container} from './ControllerRunner';
 
+var _container: Container;
+
+/**
+ * Sets the Container object for the ControllerRunner instance.
+ * 
+ * @param container IoC Container
+ */
+export function setContainer(container: Container) {
+    _container = container;
+}
+
 /**
  * Registers all loaded actions in your express application.
  *
  * @param expressApp Express application instance
  * @param requireDirs Array of directories where from controller files will be loaded
  */
-export function registerActionsInExpressApp(expressApp: any, requireDirs?: string[], container?: Container): ControllerRunner {
+export function registerActionsInExpressApp(expressApp: any, requireDirs?: string[]): ControllerRunner {
     const controllerRunner = new ControllerRunner(new ExpressServer(expressApp));
-    if (container)
-        controllerRunner.container = container;
+    if (_container)
+        controllerRunner.container = _container;
     
     if (requireDirs && requireDirs.length)
         requireDirs.map(dir => controllerRunner.loadFiles(dir));
