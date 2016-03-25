a6ef266df01bcba98c94c894998d3d78e82ae1d3
Cody Simms <codyjsimms@gmail.com>
2016-03-24 19:18:54 -0500

    Added options object and interface

--- sample/sample7-container/app.ts
+++ sample/sample7-container/app.ts
@@ -1,12 +1,11 @@
 import * as express from 'express';
-import {setContainer, registerActionsInExpressApp} from '../../src/Factory';
+import {registerActionsInExpressApp} from '../../src/Factory';
 import kernel from './kernel';
 import ControllerContainer from './ControllerContainer';
 
 let app = express();
 
-setContainer(new ControllerContainer(kernel));
-registerActionsInExpressApp(app);
+registerActionsInExpressApp(app, {requireDirs: [__dirname + "/controllers"], container: new ControllerContainer(kernel)});
 
 app.listen(3001, 'localhost');
 console.log('Listening on port 3001. Visit http://localhost:3001/respond');
--- src/Factory.ts
+++ src/Factory.ts
@@ -2,15 +2,9 @@ import {ControllerRunner} from "./ControllerRunner";
 import {ExpressServer} from "./server/ExpressServer";
 import {Container} from './ControllerRunner';
 
-var _container: Container;
-
-/**
- * Sets the Container object for the ControllerRunner instance.
- * 
- * @param container IoC Container
- */
-export function setContainer(container: Container) {
-    _container = container;
+export interface FactoryOptions {
+    requireDirs?: string[];
+    container?: Container;
 }
 
 /**
@@ -18,14 +12,15 @@ export function setContainer(container: Container) {
  *
  * @param expressApp Express application instance
  * @param requireDirs Array of directories where from controller files will be loaded
+ * @param container Container to load objects from
  */
-export function registerActionsInExpressApp(expressApp: any, requireDirs?: string[]): ControllerRunner {
+export function registerActionsInExpressApp(expressApp: any, options?: FactoryOptions): ControllerRunner {
     const controllerRunner = new ControllerRunner(new ExpressServer(expressApp));
-    if (_container)
-        controllerRunner.container = _container;
+    if (options && options.container)
+        controllerRunner.container = options.container;
     
-    if (requireDirs && requireDirs.length)
-        requireDirs.map(dir => controllerRunner.loadFiles(dir));
+    if (options && options.requireDirs && options.requireDirs.length)
+        options.requireDirs.map(dir => controllerRunner.loadFiles(dir));
 
     controllerRunner.registerAllActions();
     return controllerRunner;
