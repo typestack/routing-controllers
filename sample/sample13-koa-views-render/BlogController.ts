90026d44cd07db2fd797ac812ea17aff966dcf4a
Joshua Lawson <joshua.lawson@sphyrix.net>
2017-01-13 20:00:17 +1100

    Added sample for rendering using koa-views and handlebars

---
+++ sample/sample13-koa-views-render/BlogController.ts
@@ -0,0 +1,26 @@
+import "reflect-metadata";
+import {Controller} from "../../src/decorator/controllers";
+import {Get, Post, Put, Patch, Delete} from "../../src/decorator/methods";
+import {Render} from "../../src/decorator/decorators";
+
+@Controller()
+export class UserController {
+
+    @Get("/")
+    @Render("blog.html")
+    blog() {
+        return {
+            title: "My Blog",
+            posts: [
+                {
+                    title: "Welcome to my blow",
+                    content: "This is my new blog built with Koa, routing-controllers and koa-views"
+                },
+                {
+                    title: "Hello World",
+                    content: "Hello world from Koa and routing-controllers"
+                }
+            ]
+        };
+    }
+}
