d8303d36415eb588468c7a68c522d509bdd43afc
Umed Khudoiberdiev <zarrhost@gmail.com>
2017-04-05 12:10:38 +0500

    added deprecations in RoutingControllersOptions

--- src/RoutingControllersOptions.ts
+++ src/RoutingControllersOptions.ts
@@ -7,17 +7,70 @@ import {ValidatorOptions} from "class-validator";
 export interface RoutingControllersOptions {
 
     /**
-     * List of directories from where to "require" all your controllers.
+     * Global route prefix, for example '/api'.
      */
-    controllers?: string[];
+    routePrefix?: string;
 
     /**
-     * List of directories from where to "require" all your middlewares.
+     * List of controllers to register in the framework or directories from where to import all your controllers.
      */
-    middlewares?: string[];
+    controllers?: Function[]|string[];
+
+    /**
+     * List of middlewares to register in the framework or directories from where to import all your middlewares.
+     */
+    middlewares?: Function[]|string[];
+
+    /**
+     * Indicates if constructor-utils should be used to perform serialization / deserialization.
+     */
+    classTransformer?: boolean;
+
+    /**
+     * Global class transformer options passed to class-transformer during classToPlain operation.
+     * This operation is being executed when server returns response to user.
+     */
+    classToPlainTransformOptions?: ClassTransformOptions;
+
+    /**
+     * Global class transformer options passed to class-transformer during plainToClass operation.
+     * This operation is being executed when parsing user parameters.
+     */
+    plainToClassTransformOptions?: ClassTransformOptions;
+
+    /**
+     * Indicates if class-validator should be used to auto validate objects injected into params.
+     */
+    validator?: boolean;
+
+    /**
+     * Global class-validator options passed during validate operation.
+     */
+    validatorOptions?: ValidatorOptions;
+
+    /**
+     * Indicates if development mode is enabled.
+     * By default its enabled if your NODE_ENV is not equal to "production".
+     */
+    development?: boolean;
+
+    /**
+     * Indicates if default routing-controller's error handler is enabled or not.
+     * Enabled by default.
+     */
+    defaultErrorHandler?: boolean;
+
+    /**
+     * Map of error overrides.
+     */
+    errorOverridingMap?: { [key: string]: any };
+
+    // deprecated API:
 
     /**
      * List of directories from where to "require" all your interceptors.
+     *
+     * @deprecated interceptors functionality is deprecated
      */
     interceptors?: string[];
 
@@ -38,55 +91,39 @@ export interface RoutingControllersOptions {
     /**
      * List of directories from where to "require" all your interceptors.
      *
-     * @deprecated Use interceptors instead.
+     * @deprecated interceptors functionality is deprecated
      */
     interceptorDirs?: string[];
 
     /**
      * Indicates if constructor-utils should be used to perform serialization / deserialization.
+     *
+     * @deprecated use classTransformer option instead
+     * @deprecated name is not consistent with "enableValidation" option but does the same thing but for another module
      */
     useClassTransformer?: boolean;
 
     /**
      * Indicates if class-validator should be used to auto validate objects injected into params.
+     *
+     * @deprecated use validator option instead
+     * @deprecated name is not consistent with "useClassTransformer" option but does the same thing but for another module
      */
     enableValidation?: boolean;
 
     /**
      * Global class-validator options passed during validate operation.
+     *
+     * @deprecated use validatorOptions instead - name does not represent what is inside and confusing because there is ValidationOptions object too.
      */
     validationOptions?: ValidatorOptions;
 
     /**
-     * Global class transformer options passed to class-transformer during classToPlain operation.
-     * This operation is being executed when server returns response to user.
-     */
-    classToPlainTransformOptions?: ClassTransformOptions;
-
-    /**
-     * Global class transformer options passed to class-transformer during plainToClass operation.
-     * This operation is being executed when parsing user parameters.
-     */
-    plainToClassTransformOptions?: ClassTransformOptions;
-
-    /**
-     * Indicates if development mode is enabled. By default its enabled if your NODE_ENV is not equal to "production".
+     * Indicates if development mode is enabled.
+     * By default its enabled if your NODE_ENV is not equal to "production".
+     *
+     * @deprecated use development option instead
      */
     developmentMode?: boolean;
-
-    /**
-     * Indicates if default routing-controller's error handler is enabled or not. By default its enabled.
-     */
-    defaultErrorHandler?: boolean;
-
-    /**
-     * Map of error overrides.
-     */
-    errorOverridingMap?: Object;
-    
-    /**
-     * Route prefix. eg '/api'
-     */
-    routePrefix?: string;
     
 }
