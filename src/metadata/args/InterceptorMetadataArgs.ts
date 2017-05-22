ea3cf7e23ab007d87235e9a44dc59219149db6cf
Umed Khudoiberdiev <zarrhost@gmail.com>
2017-05-22 12:29:37 +0500

    returned interceptor

---
+++ src/metadata/args/InterceptorMetadataArgs.ts
@@ -0,0 +1,21 @@
+/**
+ * Metadata used to store registered interceptor.
+ */
+export interface InterceptorMetadataArgs {
+
+    /**
+     * Object class of the interceptor class.
+     */
+    target: Function;
+
+    /**
+     * Indicates if this interceptor is global, thous applied to all routes.
+     */
+    global: boolean;
+
+    /**
+     * Execution priority of the interceptor.
+     */
+    priority: number;
+
+}
