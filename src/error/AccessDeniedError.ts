826a3aa67426619a6a486dceb7f506d2c5789b24
Diluka W <pmars42@gmail.com>
2017-06-08 16:24:30 +0800

    fix: AccessDeniedError should be with http status 403

--- src/error/AccessDeniedError.ts
+++ src/error/AccessDeniedError.ts
@@ -1,15 +1,15 @@
 import {Action} from "../Action";
-import {UnauthorizedError} from "../http-error/UnauthorizedError";
+import {HttpError} from "../http-error/HttpError";
 
 /**
  * Thrown when route is guarded by @Authorized decorator.
  */
-export class AccessDeniedError extends UnauthorizedError {
+export class AccessDeniedError extends HttpError {
 
     name = "AccessDeniedError";
 
     constructor(action: Action) {
-        super();
+        super(403);
         Object.setPrototypeOf(this, AccessDeniedError.prototype);
         const uri = action.request.method + " " + action.request.url; // todo: check it it works in koa
         this.message = `Access is denied for request on ${uri}`;
