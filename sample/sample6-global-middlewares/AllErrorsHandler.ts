470146a088c1745833351100b476d106e4e955bc
Matthias Vogel <git@kanti.de>
2017-11-08 19:40:43 +0100

    Update AllErrorsHandler.ts

--- sample/sample6-global-middlewares/AllErrorsHandler.ts
+++ sample/sample6-global-middlewares/AllErrorsHandler.ts
@@ -1,10 +1,10 @@
 import {ExpressErrorMiddlewareInterface} from "../../src/driver/express/ExpressErrorMiddlewareInterface";
 import {Middleware} from "../../src/decorator/Middleware";
 
-@Middleware({ type: "before" })
+@Middleware({ type: "after" })
 export class AllErrorsHandler implements ExpressErrorMiddlewareInterface {
 
-    error(error: any, request: any, response: any, next?: Function): void {
+    error(error: any, request: any, response: any, next: Function): void {
         console.log("Error handled: ", error);
         next(error);
     }
