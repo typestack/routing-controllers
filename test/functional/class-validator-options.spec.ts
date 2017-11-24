644f0947adccb2b0210fd309a548636dc3ebee24
Karol Kaczmarek <karol.kaczmarek@rst.com.pl>
2017-11-24 12:47:10 +0100

    tests for paramName in BadRequestError

--- test/functional/class-validator-options.spec.ts
+++ test/functional/class-validator-options.spec.ts
@@ -8,6 +8,10 @@ import {Get} from "../../src/decorator/Get";
 import {QueryParam} from "../../src/decorator/QueryParam";
 import {ResponseClassTransformOptions} from "../../src/decorator/ResponseClassTransformOptions";
 import {RoutingControllersOptions} from "../../src/RoutingControllersOptions";
+import {ExpressErrorMiddlewareInterface} from "../../src/driver/express/ExpressErrorMiddlewareInterface";
+import {Middleware} from "../../src/decorator/Middleware";
+import {KoaMiddlewareInterface} from "../../src/driver/koa/KoaMiddlewareInterface";
+
 const chakram = require("chakram");
 const expect = chakram.expect;
 
@@ -205,6 +209,82 @@ describe("parameters auto-validation", () => {
                 keyword: "Umedi",
                 __somethingPrivate: "blablabla",
             });
+        });
+    });
+
+    describe("should contain param name on validation failed", () => {
+
+        let requestFilter: any;
+        beforeEach(() => {
+            requestFilter = undefined;
+        });
+
+        before(() => {
+            getMetadataArgsStorage().reset();
+
+            @JsonController()
+            class UserController {
+
+                @Get("/user")
+                getUsers(@QueryParam("filter") filter: UserFilter): any {
+                    requestFilter = filter;
+                    const user = new UserModel();
+                    user.id = 1;
+                    user._firstName = "Umed";
+                    user._lastName = "Khudoiberdiev";
+                    return user;
+                }
+            }
+        });
+
+        const options: RoutingControllersOptions = {
+            validation: true,
+            defaultErrorHandler: false
+        };
+
+        let expressApp: any, koaApp: any;
+        before(done => {
+
+            @Middleware({type: "after"})
+            class ExpressErrorHandler implements ExpressErrorMiddlewareInterface {
+
+                error(error: any, request: any, response: any, next: (err?: any) => any): void {
+                    response.statusCode = 400;
+                    response.send(error.paramName);
+                    next();
+                }
+            }
+
+            expressApp = createExpressServer(options)
+                .listen(3001, done);
+        });
+        after(done => expressApp.close(done));
+        before(done => {
+
+            @Middleware({type: "before"})
+            class KoaErrorHandler implements KoaMiddlewareInterface {
+
+                async use(context: any, next: (err?: any) => Promise<any>): Promise<any> {
+                    try {
+                        await next();
+                    } catch (e) {
+                        context.body = e.paramName;
+                        context.status = 400;
+                    }
+                }
+            }
+
+            koaApp = createKoaServer(options).listen(3002, done);
+        });
+        after(done => koaApp.close(done));
+
+        const invalidFilter = {
+            keyword: "aa"
+        };
+
+        assertRequest([3001, 3002], "get", `user?filter=${JSON.stringify(invalidFilter)}`, response => {
+            expect(response).to.have.status(400);
+            expect(response.body).to.equal("filter");
         });
     });
 
