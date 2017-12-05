778ea0ba8db8e060c92dfc5a5bbe9029d5c7b826
Aleksi Pekkala <aleksi.v.a.pekkala@student.jyu.fi>
2017-12-05 12:30:12 +0200

    Implement TransformationOptions instead of extending it in controller/action options

--- src/decorator-options/ControllerOptions.ts
+++ src/decorator-options/ControllerOptions.ts
@@ -1,8 +1,14 @@
-import {TransformationOptions} from "../TransformationOptions";
-
 /**
  * Extra options that apply to each controller action.
  */
-export interface ControllerOptions extends TransformationOptions {
+export interface ControllerOptions {
+    /**
+     * If set to false, class-transformer won't be used to perform request serialization.
+     */
+    transformRequest?: boolean;
 
+    /**
+     * If set to false, class-transformer won't be used to perform response serialization.
+     */
+    transformResponse?: boolean;
 }
--- src/decorator-options/HandlerOptions.ts
+++ src/decorator-options/HandlerOptions.ts
@@ -1,8 +1,14 @@
-import {TransformationOptions} from "../TransformationOptions";
-
 /**
  * Extra handler-specific options.
  */
-export interface HandlerOptions extends TransformationOptions {
+export interface HandlerOptions {
+    /**
+     * If set to false, class-transformer won't be used to perform request serialization.
+     */
+    transformRequest?: boolean;
 
+    /**
+     * If set to false, class-transformer won't be used to perform response serialization.
+     */
+    transformResponse?: boolean;
 }
