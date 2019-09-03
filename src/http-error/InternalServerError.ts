f18d2c42aa28031a82688533d0d982c989b3b6a3
Kevin Mircovich <kmircovich1@gmail.com>
2019-09-03 13:01:15 -0400

    "message" param is not required in constructor

--- src/http-error/InternalServerError.ts
+++ src/http-error/InternalServerError.ts
@@ -6,7 +6,7 @@ import {HttpError} from "./HttpError";
 export class InternalServerError extends HttpError {
     name = "InternalServerError";
 
-    constructor(message: string) {
+    constructor(message?: string) {
         super(500);
         Object.setPrototypeOf(this, InternalServerError.prototype);
 
