b4bf77ce0cba3b8b1dec8f8d9e6389b2a8de994c
jotamorais <morais.jonathas@gmail.com>
2020-05-28 18:11:51 -0500

    Fix issue after merging PR#329

--- src/decorator/All.ts
+++ src/decorator/All.ts
@@ -1,4 +1,5 @@
 import {getMetadataArgsStorage} from "../index";
+import { ControllerOptions } from "../decorator-options/ControllerOptions";
 
 /**
  * Registers an action to be executed when a request comes on a given route.
@@ -16,13 +17,14 @@ export function All(route?: string): Function;
  * Registers an action to be executed when a request comes on a given route.
  * Must be applied on a controller action.
  */
-export function All(route?: string|RegExp): Function {
+export function All(route?: string|RegExp, options?: ControllerOptions): Function {
     return function (object: Object, methodName: string) {
         getMetadataArgsStorage().actions.push({
             type: "all",
             target: object.constructor,
             method: methodName,
-            route: route
+            route: route,
+            options,
         });
     };
 }
