3618cf853a1bd13321116e05d0d13e1c1e0fefd3
Ivan Proskuryakov <volgodark@gmail.com>
2020-06-17 12:01:15 +0300

    fixing tests, flushing & building MetadataArgsStorage in the begging of the each test case

--- test/unit/controller-inheritance.spec.ts
+++ test/unit/controller-inheritance.spec.ts
@@ -1,39 +1,41 @@
 import "reflect-metadata";
 import {MetadataBuilder} from "../../src/metadata-builder/MetadataBuilder";
-
-import {Post} from "../../src/decorator/Post";
-import {Controller} from "../../src";
+import {Controller, getMetadataArgsStorage, Post} from "../../src";
 
 const expect = require("chakram").expect;
 
 describe("controller inheritance", () => {
-    const metadataBuilder = new MetadataBuilder({});
-
-    abstract class AbstractControllerTemplate {
-        @Post()
-        public create() {
-        }
-    }
-
-    @Controller(`/derivative`)
-    class DerivativeController extends AbstractControllerTemplate {
-    }
-
-    @Controller(`/autonomous`)
-    class AutonomousController {
-        @Post()
-        public create() {
-        }
-    }
-
 
     it("should build empty meta for empty set", () => {
+        // Reset storage
+        getMetadataArgsStorage().reset();
+        const metadataBuilder = new MetadataBuilder({});
         const meta = metadataBuilder.buildControllerMetadata([]);
 
         expect(meta.length).to.be.eq(0);
     });
 
-    it("should build meta if only derivative controller given", () => {
+    it("should build meta if the only derivative controller is given", () => {
+        // Reset storage
+        getMetadataArgsStorage().reset();
+
+        // Persist storage from decorators
+        abstract class AbstractControllerTemplate {
+            @Post()
+            public create() {}
+        }
+
+        @Controller(`/derivative`)
+        class DerivativeController extends AbstractControllerTemplate {}
+
+        @Controller(`/autonomous`)
+        class AutonomousController {
+            @Post()
+            public create() {}
+        }
+
+        // Build controllers
+        const metadataBuilder = new MetadataBuilder({});
         const meta = metadataBuilder.buildControllerMetadata([
             DerivativeController,
         ]);
@@ -46,7 +48,26 @@ describe("controller inheritance", () => {
         expect(meta[0].actions[0].type).to.be.eq("post");
     });
 
-    it("should build meta if only autonomous controller given", () => {
+    it("should build meta if the only autonomous controller is given", () => {
+        getMetadataArgsStorage().reset();
+
+        // Persist storage from decorators
+        abstract class AbstractControllerTemplate {
+            @Post()
+            public create() {}
+        }
+
+        @Controller(`/derivative`)
+        class DerivativeController extends AbstractControllerTemplate {}
+
+        @Controller(`/autonomous`)
+        class AutonomousController {
+            @Post()
+            public create() {}
+        }
+
+        // Build controllers
+        const metadataBuilder = new MetadataBuilder({});
         const meta = metadataBuilder.buildControllerMetadata([
             AutonomousController,
         ]);
@@ -59,11 +80,27 @@ describe("controller inheritance", () => {
         expect(meta[0].actions[0].type).to.be.eq("post");
     });
 
-    it("should build meta if autonomous and derivative controllers given", () => {
-        const meta = metadataBuilder.buildControllerMetadata([
-            DerivativeController,
-            AutonomousController,
-        ]);
+    it("should build meta both when autonomous and derivative controllers are given", () => {
+        getMetadataArgsStorage().reset();
+
+        // Persist storage from decorators
+        abstract class AbstractControllerTemplate {
+            @Post()
+            public create() {}
+        }
+
+        @Controller(`/derivative`)
+        class DerivativeController extends AbstractControllerTemplate {}
+
+        @Controller(`/autonomous`)
+        class AutonomousController {
+            @Post()
+            public create() {}
+        }
+
+        // Build controllers
+        const metadataBuilder = new MetadataBuilder({});
+        const meta = metadataBuilder.buildControllerMetadata();
 
         expect(meta.length).to.be.eq(2);
         expect(meta[0].actions.length).to.be.eq(1);
@@ -74,5 +111,4 @@ describe("controller inheritance", () => {
         expect(meta[1].actions[0].method).to.be.eq("create");
         expect(meta[1].actions[0].type).to.be.eq("post");
     });
-
 });
