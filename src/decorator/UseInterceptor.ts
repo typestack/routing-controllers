7d515330acbef1a821ea3f4886aaf3adf9979e18
driescroons <dries.croons@panenco.com>
2022-03-21 14:22:13 +0100

    feat: interceptor ordering

--- src/decorator/UseInterceptor.ts
+++ src/decorator/UseInterceptor.ts
@@ -1,5 +1,16 @@
 import { getMetadataArgsStorage } from '../index';
 import { Action } from '../Action';
+import { UseInterceptorMetadataArgs } from '../metadata/args/UseInterceptorMetadataArgs';
+
+/**
+ * Specifies a given interceptor middleware or interceptor function, to which UseInterceptorMetadataArgs can be applied, to be used for controller or controller action. 
+ * Must be set to controller action or controller class.
+ */
+export function UseInterceptor(
+  optionsOrInterceptor: Partial<UseInterceptorMetadataArgs> | Function,
+  ...interceptors: Array<Function>
+): Function;
+
 
 /**
  * Specifies a given interceptor middleware or interceptor function to be used for controller or controller action.
@@ -8,22 +19,40 @@ import { Action } from '../Action';
 export function UseInterceptor(...interceptors: Array<Function>): Function;
 
 /**
+ * Specifies a given interceptor middleware or interceptor function, to which UseInterceptorMetadataArgs can be applied, to be used for controller or controller action. 
+ * Must be set to controller action or controller class.
+ */
+export function UseInterceptor(
+  optionsOrInterceptor: Partial<UseInterceptorMetadataArgs> | ((action: Action, result: any) => any),
+  ...interceptors: Array<(action: Action, result: any) => any>
+): Function;
+
+/**
  * Specifies a given interceptor middleware or interceptor function to be used for controller or controller action.
  * Must be set to controller action or controller class.
  */
 export function UseInterceptor(...interceptors: Array<(action: Action, result: any) => any>): Function;
 
 /**
- * Specifies a given interceptor middleware or interceptor function to be used for controller or controller action.
+ * Specifies a given interceptor middleware or interceptor function, to which UseInterceptorMetadataArgs can be applied, to be used for controller or controller action.
  * Must be set to controller action or controller class.
  */
-export function UseInterceptor(...interceptors: Array<Function | ((action: Action, result: any) => any)>): Function {
+export function UseInterceptor(
+  optionsOrInterceptor: Partial<UseInterceptorMetadataArgs> | Function | ((action: Action, result: any) => any),
+  ...interceptors: Array<Function | ((action: Action, result: any) => any)>
+): Function {
+  const optionsIsAnInterceptor = optionsOrInterceptor instanceof Function || Array.isArray(optionsOrInterceptor);
+  const options: Partial<UseInterceptorMetadataArgs> = optionsIsAnInterceptor
+    ? {}
+    : (optionsOrInterceptor as Partial<UseInterceptorMetadataArgs>);
   return function (objectOrFunction: Object | Function, methodName?: string) {
-    interceptors.forEach(interceptor => {
+    [...(optionsIsAnInterceptor ? [optionsOrInterceptor as Function] : []), ...interceptors].forEach(interceptor => {
       getMetadataArgsStorage().useInterceptors.push({
+        ...options,
         interceptor: interceptor,
         target: methodName ? objectOrFunction.constructor : (objectOrFunction as Function),
         method: methodName,
+        priority: options.priority ?? 0,
       });
     });
   };
