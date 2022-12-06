aa6e83b769806c259dc800d20f312990540c66d8
Attila Orosz <atttila.orosz@gmail.com>
2022-12-06 01:06:23 +0100

    prettier fix

--- test/utilities/axios.ts
+++ test/utilities/axios.ts
@@ -1,7 +1,7 @@
 import Axios, { AxiosInstance } from 'axios';
-import {Agent} from "http";
+import { Agent } from 'http';
 
 export const axios: AxiosInstance = Axios.create({
   baseURL: 'http://localhost:3001/',
-  httpAgent: new Agent({keepAlive: false}) // disable keepAlive until axios is fixed
+  httpAgent: new Agent({ keepAlive: false }), // disable keepAlive until axios is fixed
 });
