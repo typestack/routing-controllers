7950142ebe0f0a1d9907950ad1dc3311e74de9e1
Attila Orosz <atttila.orosz@gmail.com>
2024-09-18 07:29:04 +0200

    chore: update outdated dependencies (#1441)

--- src/error/AccessDeniedError.ts
+++ src/error/AccessDeniedError.ts
@@ -10,7 +10,6 @@ export class AccessDeniedError extends ForbiddenError {
   constructor(action: Action) {
     super();
     Object.setPrototypeOf(this, AccessDeniedError.prototype);
-    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
     const uri = `${action.request.method} ${action.request.url}`; // todo: check it it works in koa
     this.message = `Access is denied for request on ${uri}`;
   }
